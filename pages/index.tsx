94d27a2940af883c95b456f277d3dc7dc9a07984
brianbuntz <38259519+brianbuntz@users.noreply.github.com>
2024-11-08 08:16:24 -0800

    Update index.tsx
    To update mobile card size

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 // pages/index.tsx
-
 import React from "react";
 import CategoryCard from "../components/CategoryCard";
 import topLabsData from "../data/topLabsNew.json";
@@ -9,24 +8,23 @@ const Home: React.FC = () => {
   const data: TopLabsData = topLabsData as unknown as TopLabsData;
   const categories: ResearchCategory[] = data.researchCategories;
 
-  // Function to get organization details
   const getOrganization = (orgId: string): Organization | undefined => {
     return data.organizations.find((org) => org.id === orgId);
   };
 
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-grow container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-left mb-8">
+      <main className="flex-grow container mx-auto px-2 sm:px-4 py-4 sm:py-8">
+        <h1 className="text-2xl sm:text-3xl font-bold text-left mb-4 sm:mb-8">
           Explore Labs by Domain
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-3">
           {categories.map((category, index) => (
             <CategoryCard
               key={category.id}
               category={category}
               getOrganization={getOrganization}
-              priority={index < 3} // Ensure first 3 images are not lazy-loaded
+              priority={index < 3}
             />
           ))}
         </div>
